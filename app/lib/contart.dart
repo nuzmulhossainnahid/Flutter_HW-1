8dbb7a433a38a3668123cbf6d16c6829ecc0c199
nuzmulhossainnahid <hasannahidnazmul@gmail.com>
2021-09-26 13:07:13 +0600

    V-2

--- app/lib/contart.dart
+++ app/lib/contart.dart
@@ -20,15 +20,28 @@ class ContactScreen extends StatelessWidget {
           itemCount: 60,
           itemBuilder: (BuildContext context, int index) {
             return ListTile(
-              onTap: () {},
-              leading: CircleAvatar(
-                radius: 32,
-                child: Icon(Icons.phone),
-              ),
-              title: Text("+75204820025"),
-              subtitle: Text("Flutter Developer"),
-              trailing: Icon(Icons.arrow_forward_ios),
-            );
+                onTap: () {},
+                leading: CircleAvatar(
+                  radius: 32,
+                  child: Icon(Icons.phone),
+                ),
+                title: Text("+75204820025"),
+                subtitle: Text("Flutter Developer"),
+                trailing: Container(
+                  width: 50,
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.edit,
+                        color: Colors.green,
+                      ),
+                      Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      ),
+                    ],
+                  ),
+                ));
           },
         ));
   }
